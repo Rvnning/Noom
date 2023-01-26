c97cc6564bec2f8472a858bca1f8d7a533394bfd
Rvnning <a01057565165@gmail.com>
2023-01-26 17:05:28 +0900

    chat1

--- src/server.js
+++ src/server.js
@@ -16,12 +16,14 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser");
   socket.on("close", () => console.log("Discconected to Browser"));
   socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
+    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
   });
-  socket.send("hello!");
 });
 server.listen(3000, handleListen);
